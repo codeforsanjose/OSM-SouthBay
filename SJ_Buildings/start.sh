f9ee0fd7d85c2575d17c1091ede9d092e2556c5e
Spencer Alves <impiaaa@gmail.com>
2019-09-30 23:05:18 -0700

    Fix spatial reference script

--- SJ_Buildings/start.sh
+++ SJ_Buildings/start.sh
@@ -4,11 +4,11 @@ DBNAME=svosm
 psql --echo-all --file=103240.sql ${DBNAME} postgres
 
 # Import shapefiles
-shp2pgsql -d -s 103240 -I Basemap/Parcel.shp | psql -d ${DBNAME} >/dev/null
-shp2pgsql -d -s 103240 -I Basemap2/BuildingFootprint.shp | psql -d ${DBNAME} >/dev/null
-shp2pgsql -d -s 103240 -I Basemap2/CondoParcel.shp | psql -d ${DBNAME} >/dev/null
-shp2pgsql -d -s 103240 -k -I Basemap2/Site_Address_Points.shp | psql -d ${DBNAME} >/dev/null
-shp2pgsql -d -s 103240 -I Basemap2/TractBoundary.shp | psql -d ${DBNAME} >/dev/null
+shp2pgsql -d -D -s 103240 -I Basemap/Parcel.shp | psql -d ${DBNAME} >/dev/null
+shp2pgsql -d -D -s 103240 -I Basemap2/BuildingFootprint.shp | psql -d ${DBNAME} >/dev/null
+shp2pgsql -d -D -s 103240 -I Basemap2/CondoParcel.shp | psql -d ${DBNAME} >/dev/null
+shp2pgsql -d -D -s 103240 -k -I Basemap2/Site_Address_Points.shp | psql -d ${DBNAME} >/dev/null
+shp2pgsql -d -D -s 103240 -I Basemap2/TractBoundary.shp | psql -d ${DBNAME} >/dev/null
 
 # Import OSM
 osm2pgsql --database ${DBNAME} --create --prefix osm --slim --hstore --latlong --multi-geometry --bbox "-122.030997029163,37.1409001316686,-121.668646071535,37.4620825000623" norcal-latest.osm.pbf
